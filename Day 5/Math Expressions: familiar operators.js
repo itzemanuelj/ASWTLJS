06ec0f4b8cd732758e7100e18cebb8e58615fa19
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-25 13:52:06 -0500

    mvp commit for Day 5

---
+++ Day 5/Math Expressions: familiar operators.js
@@ -0,0 +1,47 @@
+///Enter the operators, in this order, for addition, subtraction, multiplication, and division. Don't type spaces between them.
+
+//!!!+ - * /
+
+/// What is the name of the arithmetic operator that gives you the remainder when one number is divided by another? (one word)
+
+let time = 5 % 2;
+/// modlus
+
+///What is the value of num?
+let num = 20 % 6;
+console.log(num);
+/// 2   
+
+
+
+const costPerItem = totalCost / numberOfItems;
+
+/// In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000
+
+let largeNum = 10000 * 20000;
+
+/// Assign to the variable timer the result of dividing 9 by the value represented by the variable qty. The variable num hasn't been declared beforehand.
+timer = 2;
+let timer = 9 / qty;
+
+/// Assign to a variable the result of adding a number to itself, for example 2 + 2. The variable hasn't been declared beforehand. Make up the variable name.
+
+let addition = 2 + 2;
+
+/// Assign to a variable the value represented by one variable subtracted from the value represented by another variable. The first variable has been declared beforehand. Make up all the variable names.
+
+let subtract = sub1 = sub2; 
+
+/// Code an alert that displays the result of any mathematical operation on 2 variables. Make up the variable names.
+let subtract3 = sub1 - sub2; 
+
+/// or
+
+alert(sub1 - sub2);
+
+/// Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+
+let remainder = 10 % 2;
+
+
+                            
