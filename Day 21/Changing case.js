2c3440d69b06bf8c5d8d1bb3f4d8c35ac38bd8d6
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-25 00:49:07 -0500

    mvp commit for Day 21

---
+++ Day 21/Changing case.js
@@ -0,0 +1,48 @@
+///Type the characters that are missing from this code.
+var allLower = userInput.toLowerCase;
+
+let allLower = allLower.toLowerCase();
+
+///Type the character that is missing from this code.
+/// var allLower = userInput toLowerCase();
+
+let allLower = userInput.toLowerCase();  
+
+///Two characters in this code are incorrect. Type the two correct characters.
+var allLower = userInput.tolowercase();
+
+let allLower = userInput.toLowerCase(); ///toLowerCase
+
+///What is the value of lowerVersion? Include the quotation marks in your answer.
+var lowerVersion = "NO!".toLowerCase();
+console.log(lowerVersion);
+///no!
+
+cityToCheck = cityToCheck.toLowerCase();
+
+///Convert the string represented by upperString to lower-case and assign the result to lowerString, which hasn't been declared beforehand.
+
+let lowerString = upperString.toLowerCase();
+
+///Convert the string represented by x to lower-case and assign the result to the same variable.
+
+let x = 'X'.toLocaleLowerCase();
+
+/// Convert the string represented by a variable to lower-case and assign the result to a second variable that hasn't been declared beforehand.
+fname = 'Justin';
+fname = lname.toLowerCase();
+
+/// Convert the string represented by a variable to upper-case and assign the result to a second variable that hasn't been declared beforehand.
+
+lname = 'Justin';
+lname = mname.toUpperCase();
+
+///Convert a string to lower-case and assign the result to a variable that has been declared beforehand.
+
+let city = 'DETROIT'.toLocaleLowerCase();
+
+/// Display in an alert the upper-case version of the string represented by a variable.
+alert('MICHIGAN'.toLowerCase());
+
+///Convert the string represented by an array element to lower-case and assign it to a variable that hasn't been declared beforehand.
+let pets = dog[1].toLowerCase();
