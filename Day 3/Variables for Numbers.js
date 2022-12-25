019a77b1401b6d59d014329661badc303a99b048
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-25 00:23:22 -0500

    mvp commit for Day 3

---
+++ Day 3/Variables for Numbers.js
@@ -0,0 +1,46 @@
+///You can assign a string to a variable. You can also assign a ______.
+/// number
+
+/// If a number is enclosed in quotes, it's a ______.
+let number = "2";
+///string
+
+///What kind of operation can be done on a number variable that, in one particular case, can't be done on a string variable
+let total = 1 + 1;
+///math
+
+/// What is the value of orderTotal?
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+///110
+
+let niceBigNumber = 1000000;
+
+///In a single statement, declare the variable numberAsString and assign the number 3 to it as a string.
+let numberAsString = '3';
+
+///Assign to the variable caseQty, which has already been declared, the value 144.
+let caseQty = 144;
+
+/// In a single statement declare a variable and assign a number to it. (You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to.)
+let number10;
+number10 = 10;
+
+///In a single statement declare a variable and assign to it the sum of 2 other variables. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+let sum = total + cost;
+
+///Rewrite this statement so the variable can be used in a math operation.
+var num = "9";
+
+let num = 9;
+
+///Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand. 
+let cost = 2 + 1;
+
+///In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+let totalcost;
+totalcost = 1 + 1;
+
