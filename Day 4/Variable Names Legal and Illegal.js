41fa50d89dddaebdfa6d7ef7b1c946e1c94eb4af
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-25 00:33:16 -0500

    mvp  commit for Day 4

---
+++ Day 4/Variable Names Legal and Illegal.js
@@ -0,0 +1,46 @@
+///Type the illegal character in this statement.
+// var product cost = 3.45;
+let productCost = 3.45;
+
+/// Type the illegal character in this statement.
+/// var P2_a%88$;
+
+let p2_a$;
+
+///Rewrite this using camelCase.
+var Nameofband;
+let nameOfBrand;
+
+
+///Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.
+/// let *%$
+
+let  $_;  let _$;
+
+let camelCaseQty;
+
+/// In a single statement, declare the variable firstLegalChar and assign it a string
+
+firstLegalChar = "$#@";
+
+///Declare a variable, in camelCase, that combines the two syllables boo and hoo.
+let bOOhoo = 'spooky';
+
+///In a single statement declare a legally-named variable and assign a number to it.
+let cost = 2025;
+
+///In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+let costTotal = cost + total;
+
+///The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+
+let woogieBoogie;
+let boogieWoogie = woogieBoogie;
+
+///Declare a variable that is a combination of your first and last names. Use camelCase.
+let justinJones;
+
+
+
+
+
