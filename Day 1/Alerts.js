960ae4ab51c54ce79d4066a109e4d77a42479eea
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-24 20:25:01 -0500

    mvp commit for day 2

---
+++ Day 1/Alerts.js
@@ -0,0 +1,33 @@
+/// What is the keyword that displays a message box? 
+alert('hello world!');
+
+/// What is the keyword that displays a message box? 
+let a = 1;
+
+/// Type the 2 characters that are missing in this statement.
+/// alert"You're learning JavaScript!";
+
+alert('Your learing Javascript');
+
+///A __________ ends in a semicolon.
+/// statement
+
+alert('Hi there sailor');
+
+///Code an alert that displays the message "Thanks for everything!"
+alert('Thanks for everything!');
+
+///Code an alert that displays the message "Boo!"
+alert('Boo!');
+
+///The code displays a box with a message in it. Fill in the blank.
+///______("You won!");
+alert('You won!');
+
+///Code an alert that displays the message "Hi"
+alert('Hi!');
+
+///Code an alert statement displaying any message you like.
+alert('December 24th 8:14PM');
+
+
---
+++ Day 2/Variables for Strings.js
@@ -0,0 +1,51 @@
+///In the following code, "Mark" is a string. What is name (answer with 1 word)?
+
+var name = "Mark";
+
+///variable
+
+///In one word, if it is enclosed in quotation marks, what is it?
+/// string
+
+///What is the keyword that declares a variable?
+let a = 1;
+
+/// var let const 
+
+///This statement...
+userName = "buddy5000";
+///...is correct only if the variable has already been _______.
+
+///declared
+
+///code onto the screen to form a statement that declares the variable punchline and assigns a value to it.
+let punchline = 'joke';
+
+///In a single statement, declare the variable nickname and assign the string "Satchmo" to it.
+let nickname = "Satchmo";
+
+///Declare the variable teamName.
+let teamName;
+teamName = "Detroit Pistons"
+
+///In a single statement declare a variable and assign a string of your choice to it. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+let fname = 'Justin';
+
+///Declare a variable. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+let lname;
+lname = 'Jones';
+
+///This statement has already been coded.
+var bestMan = "Charlie";
+///Assign the variable a new string.
+
+bestMan = `Cha'Chovar`;
+
+///Declare a variable without defining it. Then, in a second statement, assign it a string. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+let sportsTeam;
+sportsTeam = 'Detroit Pistons';
+
+
+
+
