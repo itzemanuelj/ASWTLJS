67a21183dcd27b7cfc79b631d8e2898140dda6cc
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-25 13:28:30 -0500

    mvp commit for day 22

--- Day 22/ Strings: measuring length and extracting parts.js
+++ Day 22/ Strings: measuring length and extracting parts.js
@@ -2,9 +2,47 @@
 let a = a.splice;
 
 ///"captain" has been assigned to someWord. You want to slice "ap" out of it. Fill in the blank.
+
 var segment = someWord.slice(1, 3);
+
 //! The slice begins with the index-1 (the second) character and ends with the character before the index-3 (the fourth) character.
 
 /// You want to take a slice out of the string that starts at the fifth character and goes to the end. Fill in the blank.
+
 var segment = someWord.slice(4);
+
 //!The fifth character is index-4. When you provide no second index, JavaScript includes all character to the end.
+
+///The number of characters in the string will be assigned to the variable. Fill in the blank.
+
+var numChars = someWord._______;
+let numChars = someWord.length;
+
+let animal = 'elephant';
+let seg = animaal.slice(1, 4);
+
+/// The string "boxer" has been assigned to the array element athletes[3]. Slice "x" out of the string and assign it to the variable seg, which hasn't been declared beforehand.
+
+let boxer = athletes[3].slice(2, 3);
+
+/// Slice a segment starting with the 10th character and going all the way to the end, from the string represented by the variable x. Assign the segment to the variable y, which hasn't been declared beforehand.
+
+let y = x.slice(9);
+
+/// The string "elephant" has been assigned to the variable animal. Slice the four middle characters out of the string and assign it to the variable seg, which hasn't been declared beforehand.
+
+animal = 'elephant';
+half = animal.slice(2, 6); //!start at index stop at index
+
+///Find the number of characters in the string represented by a variable and assign the number to a second variable.
+
+let names = lnames.lenght;
+
+///Assign the second through last characters of a variable to a second variable that hasn't been declared beforehand.
+
+let fname = middleName.slice(0, 1);
+
+///!!! In a first statement measure how many characters there are in a string represented by a variable. In a second statement slice all but the first and last characters of the string and assign it to a second variable that hasn't been declared beforehand.
+
+let cityTotal = city.lenght;
+let cityName = cityTotal.slice(0,cityTotal -1 );                       
