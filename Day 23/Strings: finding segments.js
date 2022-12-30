d792f32ac9f3b3928eae84474773d9daf79a553d
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-30 14:39:51 -0500

    mvp commit for Day 24

---
+++ Day 23/Strings: finding segments.js
@@ -0,0 +1,8 @@
+///What is the keyword for finding the index of the first character of a segment?
+
+let a = b.indexOf();
+
+///What is the keyword for finding the index of the first character of the last instance of a segment?
+
+let c = b.indexOfLast();
+
---
+++ Day 24/ Strings: finding a character at a location.js
@@ -0,0 +1,49 @@
+/// What is the keyword for finding a character at a particular location in a string?
+let name = array.charAt(0).toLowerCase();
+///In this string, what character is at index 2?—
+("abcde");
+///Don't include quotation marks in your answer.
+/// letter c
+
+///Type the characters that are incorrect.
+let cha = sentence.charAt[10];
+///parenthis not brackets
+
+///The last character of the string is assigned to the variable. Fill in the blank, using the string length to specify the index.
+var lastChar = str.charAt(________);
+let lastChar = str.charAt(str.length - 1);
+
+///form a statement that finds the first character in the string speech and assigns it to firstChar.
+firstChar = speech.charAt(0);
+
+///Find the 10th character in the string represented by text and assign it to the variable cha, which hasn't been declared beforehand.
+
+let chaa = text.charAt(9);
+
+///Find the last character in the string represented by str and assign it to x, which hasn't been declared beforehand.
+
+let str = x.charAt(str.length - 1);
+
+///Find the the first character in a string represented by str and assign it to x, which hasn't been declared beforehand.
+
+let x = str1.charAt(0);
+
+///Find the the 5th character in a string represented by input and assign it to cha, which hasn't been declared beforehand.
+
+let chat = input.charAt(4);
+
+///Find the last character in a string represented by a variable and assign it to a variable that hasn't been declared beforehand.
+
+let fnmae = list.charAt(list.length - 1);
+
+///Code the first line of an if statement that tests whether the 3rd character of a string represented by a variable is a particular character.
+
+if (teamName.charAt(2) === "a") {
+  console.log(teamName);
+}
+
+///Code a for loop that cycles through all the characters of a string represented by a variable and assigns each character to an element of an array that has been declared beforehand.
+
+for (let i = 0; i < text.length; i++) {
+  textArray[i] = text.charAt(i);
+}
