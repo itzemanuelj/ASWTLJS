336db66614ccc8e0c4b34dc98914d886678c98c0
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-31 13:59:12 -0500

    mvp commit for Day 16

---
+++ Day 16/Arrays: adding and removing elements.js
@@ -0,0 +1,37 @@
+//pop removes last element
+//push adds at the end of the element
+
+///This code declares an empty array. Complete it.
+let finalScores = [];
+
+///counties is an empty array. What is the value of counties[0]?
+countries[0] = [];
+///undefined
+
+///What keyword removes the last element from an array?
+let a = fnames.pop();
+
+///What keyword adds one or more elements to the end of an array?
+let b = lnames.push();
+
+///Assign the number 1 to the 10th element of the array nums.
+nums[9] = 1;
+
+///Remove the last element from the array pets.
+let newPetsArray = pets.pop("dog");
+
+///create an empty array
+let emptyArray = [];
+
+///Change the value of the first element of an array to a new string.
+let firstName = names[0].pop("Jones");
+
+/// Add a new element, a number, to the end of an array.
+let newElement = number.push(1);
+
+///Assign string values to the second and fourth elements of an array.
+names[1] = "Sue";
+names[3] = "Kate";
+
+/// Add three number elements to end of an array.
+let newNumbers = numbers.push(1, 2, 3);
