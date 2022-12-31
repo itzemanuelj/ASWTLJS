dcc4eaa92797f031172c019988f3f9f4ca14e487
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-31 14:15:20 -0500

    mvp commit for w 3schools

---
+++ Day 16/w3 push pop method.js
@@ -0,0 +1,23 @@
+/// Definition and Usage
+/// The pop() method removes (pops) the last element of an array.
+
+/// The pop() method changes the original array.
+
+/// The pop() method returns the removed element.
+
+/// remove the last element of the array and assign to a new variable
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let newFruits = fruits.pop();
+
+/// Definition and Usage
+/// The push() method adds new items to the end of an array.
+
+/// The push() method changes the length of the array.
+
+/// The push() method returns the new length.
+
+let fruitsArray = ["Banana", "Orange", "Apple", "Mango"];
+let newFruitArray = fruitsArray.push("Kiwi", "Limes");
+console.log(newFruitArray);
