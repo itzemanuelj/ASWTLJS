fe57cf247c0ddd8a80dbdd7af9581533bc901c2d
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-31 12:38:54 -0500

    Create Strings: replacing characters.js

---
+++ Day 25/Strings: replacing characters.js
@@ -0,0 +1,11 @@
+// let str = "JavaScript replace method test";
+// let res = str.replace("test", "success");
+// //res = Javscript replace method success
+
+// let string = "golden vigor doers goat";
+// let replacedString = string.replace("go", "GO");
+// console.log(replacedString);
+
+/// What is the keyword for removing one set of characters and inserting another set in its place?
+
+let a = team.replace();
