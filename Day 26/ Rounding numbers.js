554682155e45ba480397629b0b939f76a0bfab38
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-07 14:30:10 -0500

    mvp commit for day 26

---
+++ Day 26/ Rounding numbers.js
@@ -0,0 +1,50 @@
+/// round to the nearest number
+let roundedNum = math.round(fractionalNum);
+
+/// round up
+let roundedNumb = math.celi(fractionalNum);
+
+/// round down
+let roundedNumbe = math.floor(fractionalNum);
+
+/// What is the value of roundedNum?
+let roundedNumber = Math.floor(1.9);
+console.log(roundedNumber);
+/// 1
+
+let number = math.round(1.3);
+
+///Round up a number represented by originalNum and assign it to roundNum, which hasn't been declared beforehand.
+
+let roundNum = math.celi(originalNum);
+
+/// Round down a number represented by originalNum and assign it to roundNum, which hasn't been declared beforehand.
+
+let roundDown = math.floor(originalNum);
+
+/// Round a number represented by a variable and assign the result to a second variable that hasn't been declared beforehand.
+
+let cost = math.round(cost);
+
+/// Round down a number represented by a variable and assign the result to a second variable that hasn't been declared beforehand.
+
+let total = math.floor(cost);
+
+/// Round up a number represented by a variable and assign the result to a second variable that hasn't been declared beforehand
+
+let intrest = math.celi(cost);
+
+///  Round .5 to 0 and assign it to a variable that hasn't been declared beforehand.
+
+let num = 0.5;
+
+let numFloor = math.floor(num);
+console.log(num);
+/// 0
+
+// or
+// let num = Math.floor(0.5);
+
+/// Round .00001 to 1 and assign it to a variable that hasn't been declared beforehand.
+
+let numberCeli = Math.celi(0.00001);
