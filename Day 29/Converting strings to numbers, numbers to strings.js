b9b87e2bda0fda0de1ceb4118c460bb01f54c78a
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-11 13:28:44 -0500

    Create Converting strings to numbers, numbers to strings.js

---
+++ Day 29/Converting strings to numbers, numbers to strings.js
@@ -0,0 +1,41 @@
+/// What is the keyword for converting a number to a string?
+let number = 10;
+number = toString(number);
+console.log(number);
+
+/// num1 is "1", num2 is "3". Rewrite this statement so sum is assigned 4.
+num1 = "1";
+num2 = "3";
+
+let sum = Number(num1) + Number(num2);
+console.log(sum);
+///4
+
+var sums = (num1 + num2).toString();
+
+/// Convert the string represented by numAsString to a number and assign it to nums, which hasn't been declared beforehand.
+let nums = Number(numAsString);
+
+/// Convert the number represented by number to a string and assign the string to numStr, which hasn't been declared beforehand.
+
+let numStr = number.toString();
+
+/// Convert a string represented by myStr to a number and assign it to num, which hasn't been declared beforehand.
+
+let num = Number(myStr);
+
+/// Convert a number represented by num to a string and assign it to myStr, which hasn't been declared beforehand.
+
+let myStr = num.toString();
+
+/// Convert a string represented by a variable to a number and assign it to a variable that hasn't been declared beforehand.
+
+let teamYears = Number(yearsToal);
+
+/// Convert a number represented by a variable to a string and assign it to a variable that hasn't been declared beforehand.
+
+let years = yeartoal.toString();
+
+/// Code the first line of an if statement that tests whether a string represented by a variable, converted to a number, is greater than 1.
+if (Number(num1) > 1) {
+}
