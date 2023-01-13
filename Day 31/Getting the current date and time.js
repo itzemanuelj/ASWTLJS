f8c21da12f2413bba566f5ca7344e892435d42c7
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-13 13:31:33 -0500

    Create Getting the current date and time.js

---
+++ Day 31/Getting the current date and time.js
@@ -0,0 +1,62 @@
+let currentdate = new Date();
+let datetime =
+  "Last Sync: " +
+  currentdate.getDate() +
+  "/" +
+  (currentdate.getMonth() + 1) +
+  "/" +
+  currentdate.getFullYear() +
+  " @ " +
+  currentdate.getHours() +
+  ":" +
+  currentdate.getMinutes() +
+  ":" +
+  currentdate.getSeconds();
+
+/// The statement below creates a Date _____.
+let now = new Date();
+
+//date object
+
+/// A date object looks like a _______ but isn't one.
+// a string representation
+
+/// Two of the characters in this statement are incorrect. Type the correct characters in order, without a space separating them.
+// var currentDateAndTime = New date();
+
+let currentDate = new Date();
+
+///If it's Saturday, what is the value of thisDay?
+let thisDay = now.getDay();
+//6 ///Days of the week range from 0 for Sunday to 6 for Saturday
+
+let today = new Date();
+
+/// Code a statement that creates a new Date object and assigns it to dObj, which hasn't been declared beforehand.
+
+let dObj = new Date();
+
+/// Code a statement that creates a new Date object, converts it to a string, and assigns the string to dStr, which hasn't been declared beforehand.
+
+let dStr = new Date().toString();
+
+/// Code a statement that creates a new Date object and assigns it to d, which hasn't been declared beforehand.
+
+let d = new Date();
+
+/// Code a statement that extracts the day of the week from a Date object represented by d and assigns it to day, which hasn't been declared beforehand.
+
+let day = d.getDay();
+
+/// Code a statement that creates a new Date object and assigns it to a variable that hasn't been declared beforehand.
+
+let dateOfTheWeek = new Date();
+
+/// Code a statement that extracts the day of the week from a Date object represented by a variable and assigns it to a variable that hasn't been declared beforehand.
+
+let dates = dDates.getDay();
+
+/// The day has been extracted from the Date object and assigned to d. The names of the days of the week have been assigned to the array dayNames. Fill in the blank.
+alert("Today is " + ___________);
+
+// dayNames[d];
