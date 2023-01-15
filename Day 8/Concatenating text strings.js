0c3e68d73758ce77ce6455c6dde15899a78289d3
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-14 23:30:47 -0500

    Create Concatenating text strings.js

---
+++ Day 8/Concatenating text strings.js
@@ -0,0 +1,17 @@
+let num = "2" + "2";
+/// What is the value of num? Include quotation marks.
+22;
+
+/// What is it called when you combine two or more strings, using the plus sign?
+let concatenation;
+
+/// message = ("Hello," + "Dolly");
+/// What is the value of message? (Include the quotation marks.)
+
+message === Hello, Dolly;
+
+alert("33" + 3);
+/// What message displays in the alert box?
+333;
+
+/// Write a statement that assigns to scribe the concatenation of the first and last names of England's greatest playwright. The variable hasn't been declared beforehand.
