a0676d7013541ac43ffdf1eb821962d3770b60c9
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-14 23:18:12 -0500

    Create Extracting parts of the date and time.js

---
+++ Day 32/Extracting parts of the date and time.js
@@ -0,0 +1,49 @@
+/// What is the keyword for extracting the day of the month?
+
+let january;
+january.getDate();
+
+/// What is the keyword for extracting the year?
+
+let year = 1995;
+year.getFullYear();
+
+/// What is the keyword for extracting the milliseconds since midnight, Jan. 1, 1970?
+
+getTime;
+
+/// What is the keyword for extracting the smallest unit of time that JavaScript recognizes?
+
+getMilliseconds;
+
+let mins = now.getMinutes();
+
+/// Code a statement that extracts the hours from the Date object represented by dObj and assigns it to hrs, which hasn't been declared beforehand.
+
+let hrs = dObj.getHours();
+
+/// Code a statement that extracts whether it's Monday, Tuesday, etc. (as a number) from the Date object represented by d and assigns it to x, which hasn't been declared beforehand.
+
+let x = d.getDate();
+
+/// The Date object is represented by d. Code a statement that displays the year in an alert.
+alert(d.getFullYear());
+
+/// The Date object is represented by now. Code the first line of an if statement that tests whether it's the last quarter of the year.
+
+if (a.getMonth() >= 8) {
+}
+
+/// Code a statement that determines the seconds since midnight, Jan. 1, 1970 and assigns it to a variable that hasn't been declared beforehand. Use any variable you like to represent the Date object. Note that I'm asking for seconds. Don't round it.
+
+let seconds = midnight.getTime() / 1000;
+
+/// Code the first line of an if statement that tests whether it's before noon. Use any variable you like to represent the Date object.
+
+if (a.getHours() < 12) {
+}
+
+/// Code the first line of an if statement that tests whether the clock is at an hour on the dot. Check minutes and then seconds. Ignore milliseconds. Use any variable you like to represent the Date object.
+
+if (d.getMinutes() === 0 && d.getSeconds() === 0) {
+}
