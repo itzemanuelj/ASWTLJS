9021543a45e4652de9999b766cd08a1e186e77b6
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-16 13:07:59 -0500

    commits to Day 9 and Day 10

---
+++ Day 10/if statements.js
@@ -0,0 +1,54 @@
+/// What are the first 4 characters of an if statement?
+
+if (a > b) {
+}
+
+if (city === "Las Vegas") {
+}
+
+/// In the above statement, what character is incorrect?
+
+if (a === x) {
+  x === y;
+}
+
+/// Code the first line of an if statement that tests whether teammates has the value of 8.
+
+if (teammates === 8) {
+}
+
+/// This is the first line of an if statement.
+
+if (firstName === "John") {
+  alert("Hi, John");
+}
+
+/// Complete the if statement. If the condition is true, display an alert that says, "Hi, John".
+
+/// Code the first line of an if statement that tests whether one variable has the same value as another.
+if (cost === total) {
+}
+
+/// Code the first line of an if statement that tests whether a variable has the value of a particular string.
+
+if (fname === "justin") {
+}
+
+/// This is the first line of an if statement:
+
+if (x === true) {
+  let z = prompt("what is z?");
+}
+
+/// Complete the statement. If the condition is true, display a box that asks the user a question. Assign the answer to a variable that hasn't been declared beforehand.
+
+///Code an if statement that tests whether pCode is "10010". If so, assign to the variable city the string "New York". city has already been declared.
+
+if (pCode === "10010") {
+  city = "New York";
+}
+
+/// Code an if statement. Test whether a variable has a particular numerical value. If so, assign a new value to that variable, as in x = 1;
+if (numberOne === 1) {
+  numberOne = 2;
+}
---
+++ Day 9/Prompts.js
@@ -0,0 +1,52 @@
+/// What is the keyword that displays a box requesting user input?
+
+let greeting = prompt("Please enter your name");
+
+/// What is the keyword in a statement that shares most of the syntax of a prompt statement?
+
+alert("helllo world");
+
+/// What do you need in order to capture a user's input in a prompt box? Answer with one word.
+
+let hello = prompt("Hello");
+
+// a variable
+
+let gender = prompt(askGender, bestGuess);
+/// In the above statement, what is optional?
+
+let bestGuess;
+
+let home = prompt("Your Planet", "Mars");
+
+/// Code a prompt with the message "Enter first name". The user's response is assigned to firstName, which hasn't been declared beforehand.
+
+let firstName = prompt("Enter first name");
+console.log(firstName);
+
+/// Code a prompt with the message "Country?" and the default answer "China". The user's response is assigned to country, which hasn't been declared beforehand.
+
+let country = prompt("Country?", "china");
+
+/// Code a prompt that specifies a string as the message. Assign the user's response to a variable that hasn't been declared beforehand.
+
+let message = prompt("good evening");
+
+/// Code a prompt that specifies a string as the message. Include a default input. Assign the user's response to a variable that hasn't been declared beforehand.
+
+let messager = ("what is your name", "justin");
+
+/// Code a prompt that specifies a variable as the message and assigns the user's response to a variable that hasn't been declared beforehand.
+
+let teamName = prompt(DetroitPistons);
+
+/// Code a prompt that specifies a variable as the message, a second variable as the default response, and assigns the user's response to a third variable that hasn't been declared beforehand.
+
+let detrotTeam = prompt(DetroitPistons, DetoitLions);
+
+/// Assign strings to two variables. Code a prompt specifying the first variable as the message and the second variable as the default response. Assign the user's response to a third variable. None of the variables have been declared beforehand.
+
+fname = "justin";
+lname = "jones";
+
+let signiture = prompt(fname, lname);
