92c0d8a210698a47a4b6797e9ff3bbf1915195ef
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-21 15:21:09 -0500

    mvp commit for comparioson operators
    javascript

---
+++ Day 11/Comparison operators.js
@@ -0,0 +1,67 @@
+/// Comparison operators
+
+/// ===, !==, >, and <= are ________ operators.
+
+// comparion operators
+
+/// What is the comparison operator for is not equal to?
+
+if (a != b) {
+  console.log("");
+}
+
+/// What is the comparison operator for is greater than?
+if (a > b) {
+  console.log("");
+}
+
+/// What is the comparison operator for is less than or equal to?
+if (a <= b) {
+  console.log("");
+}
+
+if (a !== c) {
+  a = c;
+}
+
+/// Code the first line of an if statement that tests whether num1 is greater than num2
+
+if (num1 > num2) {
+  console.log("");
+}
+
+/// Code the first line of an if statement that tests whether num1 is greater than or equal to num2.
+
+if (num1 >= num2) {
+  console.log("");
+}
+
+/// Code the first line of an if statement that tests whether one variable is unequal to another
+
+if (one !== two) {
+  console.log("");
+}
+
+/// Code the first line of an if statement that tests whether a variable is equal to a particular string.
+
+if (gender !== "male") {
+  console.log("");
+}
+
+/// Code the first line of an if statement that tests whether a variable is less than a particular number.
+
+if (one < 1) {
+  console.log("");
+}
+
+/// Code the first line of an if statement that tests whether a variable is less than a particular number.
+
+if (total > 10) {
+  console.log("");
+}
+
+/// Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+
+if (cost >= time) {
+  console.log("");
+}
