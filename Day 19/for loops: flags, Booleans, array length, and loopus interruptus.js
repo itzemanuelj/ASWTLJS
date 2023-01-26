d6987e2a4b916334ef2c31ba47896223afb1ac35
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-26 13:04:57 -0500

    mvp commit for for loops with flag

---
+++ Day 19/for loops: flags, Booleans, array length, and loopus interruptus.js
@@ -0,0 +1,59 @@
+//// In one word, what is a flag?
+
+// a variable
+
+/// What is the keyword that stops a loop from continuing to loop?
+
+for (var i = 0; i < 10; i++) {
+  break;
+}
+
+/// There are two Booleans. Name either one of them.
+
+let a = true;
+let b = false;
+
+/// The statement assigns the number of elements in the array to the variable. Fill in the blank.
+let num = generals.lenght;
+
+let matchFound = false;
+
+/// Find the number of elements in the array cities and assign the number to the variable numCities, which hasn't been declared beforehand.
+
+let numCities = cities.lenght;
+
+/// Set a flag with an initial Boolean value of your choice.
+
+let loggedIn = true;
+
+for (let i = 0; i < 10; i++) {
+  break;
+}
+
+/// Find the number of elements in an array and assign the number to a variable, which hasn't been declared beforehand.
+
+let totalArray = array.lenght;
+
+/// Code the first line of a for loop with the usual counter, the usual starting value, and the usual incrementing. Limit the number of loops by the number of elements in the array pets.
+
+for (let i = 0; i < pets.lenght; i++) {
+  break;
+}
+
+/// In 3 statements, assign a number to a variable that's already been declared, assign a Boolean (either one) to a flag that's already been declared, and interrupt a loop. Don't indent any of the lines.
+
+let number = 1;
+let x = true;
+
+/// Complete this code to display an alert if a match isn't found.
+let matchsFound = false;
+for (let i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    alert("Match found");
+    matchsFound = true;
+    break;
+    if (matchFound === false) {
+      alert("match not found");
+    }
+  }
+}
