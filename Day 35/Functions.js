643b5e50c8fd1ce68c7e9c7c62f181212c3f61fd
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-26 13:34:39 -0500

    Create Functions.js
    mvp commit for day 35 sunctions

---
+++ Day 35/Functions.js
@@ -0,0 +1,63 @@
+/// Functions
+
+/// In one word, what is a block of code that executes whenever you invoke its name?
+function Time();
+
+/// Any name that is legal for a _________ is legal for a function.
+
+// let var const while /// variable
+
+/// Write a function call that uses just 4 characters. Name it anything you like, within the limitation that the whole statement is just 4 characters.
+
+function Total();
+
+/// This is the first line of the function code. Write the statement that calls it.
+
+function doSomething() {}
+
+doSomething();
+
+
+function warnUser() {}
+
+warnUser();
+
+/// Code the first line of a function displayAlert
+function displayAlert() {}
+
+displayAlert();
+
+/// Code the second line of a function  askName that prompts the user to "Enter name" and assigns the answer to userName, which hasn't been declared beforehand.
+
+function askName() {
+    let username = prompt('Enter name')
+}
+
+/// Code the first line of a function named for Johnny Appleseed's favorite fruit.
+function favoriteFruit() {
+    let JohnnyFaveFruit = 'apple'
+}
+
+/// Code a function named say that displays an alert with your first name as the message.
+
+function say() {
+    alert('Justin');
+}
+
+/// Code a function that concatenates string1 and string2 and assigns the result to a variable that hasn't been declared beforehand. (For the sake of practice, I'm asking you to violate a good-coding rule here, which you'll learn about in Chapter 38. If you're charged with a coding crime, I'll testify to your innocence.)
+function concat() {
+    let striny =  string1 + string2;
+}
+concat();
+
+/// What are the 4 missing characters, including a space?
+
+// function doSomething
+
+function doSomething() {}
+
+/// Code a function that calls itself.
+function itsSelf() {
+    itsSelf();
+}
+
