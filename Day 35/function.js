fd87ad8b9eecc0f51a33bebe5b8dbed824982090
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-02 23:32:50 -0500

    added code to section 35

---
+++ Day 35/function.js
@@ -0,0 +1,66 @@
+// In one word, what is a block of
+// code that executes whenever you
+// invoke its name?
+// Function
+
+// Any name that is legal for
+// a _________ is legal for a function.
+// variable
+
+// Write a function call that uses just
+// 4 characters. Name it anything you
+// like, within the limitation that
+// the whole statement is just 4
+// characters.
+
+function sayHello() {}
+
+// Invoke the function
+function doSomething() {}
+doSomething();
+
+function warnUser() {}
+
+// Code the first line of a function
+// displayAlert
+function displayAlert() {}
+
+// Code a function named askName
+// that prompts the user to
+// "Enter name" and
+// assigns the answer to userName,
+// which hasn't been declared beforehand.
+
+function askName() {
+  let userName = prompt("Enter name");
+}
+
+// Code the first line of a
+// function named for
+// Johnny Appleseed's favorite fruit.
+
+function apple() {}
+
+// Code a function named say that
+// displays an alert with your
+// first name as the message.
+
+function fname() {
+  alert("Justin");
+}
+
+// Code a function that concatenates
+// string1 and string2 and
+// assigns the result to a
+// variable that hasn't been
+// declared beforehand
+
+function concat() {
+  let string3 = string1 + string2;
+}
+
+// Code a function that calls itself.
+
+function itsSelf() {
+  itsSelf();
+}
