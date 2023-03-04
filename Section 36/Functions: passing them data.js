aaa9ef238e9e594d302de3a74f13309f1fd90fe2
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-03 21:08:43 -0500

    Create Functions: passing them data.js

---
+++ Section 36/Functions: passing them data.js
@@ -0,0 +1,55 @@
+/// Functions: passing them data
+
+/// The variables in a function definition that catch the data passed to them by the calling code are called _________.
+
+/// parameters
+
+/// When there are multiple arguments and parameters, type the character that separates the individual items in the list
+
+function team(Detroit, Lions) {}
+
+team();
+
+/// The rules for naming parameters are the same as the rules for naming ______.
+
+/// variable
+
+/// When there are multiple arguments and parameters, the arguments and parameters are matched up by their (1 word) _______.
+
+/// order
+
+function doSomething(param1, param2, param3) {}
+
+/// Code a call to the function displayAlert that passes the argument "Hello world"
+
+displayAlert("Hello World");
+
+/// Code the first line of a function named concat that has 3 parameters, the first three letters of the alphabet.
+
+function concat(a, b, c) {}
+
+/// Code a function call that takes a variable, a string, and a number as arguments.
+
+allThree(one, "one", 1);
+
+/// Code the first line of a function that has 2 parameters.
+
+function twoParams(total, cost) {}
+
+/// Code a function that has 2 parameters. Concatenate them and assign the result to a variable that hasn't been declared beforehand.
+
+function concat2(a, b) {
+  let concat3 = a + b;
+}
+
+/// The function call is:
+function calc(cost, amount) {
+  calc("Funds received", 500000);
+}
+/// Code the first line of the function.
+
+/// Code a function that has three parameters. Multiply them and assign them to a variable that hasn't been declared yet.
+
+function threeMultiply(a, b, c) {
+  let newMultiply = a * b * c;
+}
