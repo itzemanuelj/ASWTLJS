4135f8650c206fe301d33b41f5f55fbad9ea7c42
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-04 19:53:47 -0500

    added template literals for section 2

--- Section 2/Variables for Strings.ts
+++ Section 2/Variables for Strings.ts
@@ -1,6 +1,6 @@
 ///In the following code, "Mark" is a string. What is name (answer with 1 word)?
 
-var name = "Mark";
+let fistName: string = "Mark";
 
 ///variable
 
@@ -8,32 +8,30 @@ var name = "Mark";
 /// string
 
 ///What is the keyword that declares a variable?
-let a = 1;
+let aa: number = 1;
 
-/// var let const 
+/// var let const
 
 ///This statement...
-userName = "buddy5000";
+let userName2: string = "buddy5000";
 ///...is correct only if the variable has already been _______.
 
 ///declared
 
 ///code onto the screen to form a statement that declares the variable punchline and assigns a value to it.
-let punchline = 'joke';
+let punchLine: string = "joke";
 
 ///In a single statement, declare the variable nickname and assign the string "Satchmo" to it.
-let nickname = "Satchmo";
+let nickname: string = "Satchmo";
 
 ///Declare the variable teamName.
-let teamName;
-teamName = "Detroit Pistons"
+let teamName1: string = "Detroit Pistons";
 
 ///In a single statement declare a variable and assign a string of your choice to it. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
-let fname = 'Justin';
+let myName: string = "Justin";
 
 ///Declare a variable. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
-let lname;
-lname = 'Jones';
+let lastName: string = "Jones";
 
 ///This statement has already been coded.
 var bestMan = "Charlie";
@@ -43,9 +41,4 @@ bestMan = `Cha'Chovar`;
 
 ///Declare a variable without defining it. Then, in a second statement, assign it a string. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
 
-let sportsTeam;
-sportsTeam = 'Detroit Pistons';
-
-
-
-
+let sportsTeam2: string = "Detroit Pistons";
