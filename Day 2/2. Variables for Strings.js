2db06df9178462e948e4494d040e03f8a97b38c7
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-12 23:02:48 -0400

    mvp commit for day 2

---
+++ Day 2/2. Variables for Strings.js
@@ -0,0 +1,40 @@
+/// 2. Variables for Strings
+
+let fname = "Mark";
+
+/// In the following code, "Mark" is a string. What is name (answer with 1 word)?
+
+/// variable
+
+/// In one word, if it is enclosed in quotation marks, what is it?
+
+let name = "justin";
+
+/// string
+
+/// What is the keyword that declares a variable?
+
+/// var let const
+
+/// this statement...
+userName = "buddy5000";
+/// is correct only if the variable has already been _______.
+
+/// declared
+
+let punchline = "the accountant";
+
+/// In a single statement, declare the variable nickname and assign the string "Satchmo" to it.
+
+let nickname = "Satchmo";
+
+/// Declare the variable teamName.
+
+let teamName = "Detroit Pistons";
+
+let lake = "great lakes";
+
+/// This statement has already been coded.
+let bestMan = "Charlie";
+/// Assign the variable a new string.
+bestMan = "Chachovar";
