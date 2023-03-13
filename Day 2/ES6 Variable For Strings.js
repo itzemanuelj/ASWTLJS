d916f516fd72eec75153ca77021b5f0446daeca8
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-12 23:11:01 -0400

    Create ES6 Variable For Strings.js

---
+++ Day 2/ES6 Variable For Strings.js
@@ -0,0 +1,14 @@
+/// Template literals (Template strings)
+
+/// multi line strings
+
+let sentence = `hello
+world
+!
+`;
+
+let name = "justin";
+
+let message = `hello ${name}`;
+
+let total = `${3 + 2} is greater than ${3 + 1}`;
